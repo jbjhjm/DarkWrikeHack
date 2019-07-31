f41ec9d82ef41cdffe3abbfd56ea65aa69ca46b8
Jannik Mewes <jannik.mewes@yoolabs.com>
2019-07-31 10:41:49 +0200

    Move editor.css into a css file

--- content.js
+++ content.js
@@ -14,18 +14,14 @@ var iframeUpdater = function() {
 		if(!iframe2) return;
 		if(iframe2.contentDocument.querySelector('.wdt-styles')) return;
 		// console.log('####### 2',iframe2, iframe2.contentDocument);
-		var css = iframe2.contentDocument.createElement('style')
+		// var url = chrome.runtime.getURL("editor.css");
+		// console.log(url);
+		var css = iframe2.contentDocument.createElement('link');
 		css.rel = "stylesheet"; 
 		css.type = "text/css"; 
 		css.className = "wdt-styles"; 
-		// css.url = chrome.runtime.getUrl('editor.css');
-		css.innerHTML = "body { background:#000!important; color:#fff }";
-		css.innerHTML += "#innerdocbody { padding-top:15px!important; } ";
-		css.innerHTML += "#innerdocbody.w3-taskview-editor { color:#fff!important; } ";
-		css.innerHTML += "#innerdocbody a, #innerdocbody a:visited { color:#33baff!important; } ";
-		css.innerHTML += ".tasklist-done { color:rgba(255,255,255,.7); text-decoration:line-through; } ";
-		css.innerHTML += ".tasklist-not-done:not(:hover) { background-image:none; position:relative; } ";
-		css.innerHTML += ".tasklist-not-done:not(:hover):before { content:\"\"; width:14px; height:14px; background:#000; border:1px solid #888; position:absolute; left:0; top: 5px; } ";
+		css.href = chrome.runtime.getURL('editor.css');
+		// css.innerHTML = "body { background:#000!important; color:#fff }";
  		// var css = '<style type="text/css">' + '.container{background:blue}; ' + '</style>';
 		var head = iframe2.contentDocument.getElementsByTagName('head')[0];
 		head.appendChild(css);
